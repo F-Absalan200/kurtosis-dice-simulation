63c50066cf5c14c757dbf2e2acd0514fffc6b4f6
F-Absalan200 <fa.absalan.27@gmail.com>
2026-06-17 11:40:17 +0330

    First commit: core project files

---
+++ kurtosis.py
@@ -0,0 +1,27 @@
+import numpy as np
+import random
+
+
+#later ver change probability to 99% and 1%
+a = [0 for _ in range(99)] 
+a.append(1)
+
+dice = [i for i in range(1,7)]
+
+def play(profit=0):
+    result = np.random.choice(a)
+    if result == 0:
+        profit += int(np.random.choice(dice))
+        return profit
+    else:
+        profit -= 350
+        return profit
+    
+def test(num:int):
+    profit = 0
+    for _ in range(num):
+        profit = play(profit)
+        
+    print(profit)
+    
+test(10000)
---
+++ kurtosis2.py
@@ -0,0 +1,21 @@
+import random as rd
+
+hundredfaced_dice = [0, 1]
+population = [99, 1]
+dice = [1, 2, 3, 4, 5, 6]
+def play():
+    first_luck = rd.choices(hundredfaced_dice, population)
+
+    if first_luck[0] == 0:
+        return rd.choice(dice)
+    else:
+        return -350
+    
+def test(number):
+    profit = 0
+    for _ in range(number):
+        profit += play()
+        
+    return profit
+
+print(test(10))
