34d8a6aa5ff758bc5427b60e019874346684b166
F-Absalan200 <fa.absalan.27@gmail.com>
2026-06-17 12:28:11 +0330

    Restructure project files into core and add app.py

--- core/simulation_v2.py
+++ core/simulation_v2.py
@@ -8,27 +8,21 @@ def run_dice_simulation(num_simulations=50000):
     steps = num_simulations
     cash = 0
     cash_sequence = [0]
-
     game_elements = [-350, 'ordinary_dice']
     probabilities = [0.01, 0.99]
 
     for i in range(steps):
         result = np.random.choice(game_elements, p=probabilities)
-        if result == '-350':  
+        if result == '-350':
             cash -= 350
             cash_sequence.append(cash)
         else:
             ordinary_dice = np.random.randint(1, 7)
             cash += ordinary_dice
             cash_sequence.append(cash)
-            
     return cash_sequence
 
 if __name__ == '__main__':
-    final_sequence = run_dice_simulation()
-    
-    plt.plot(final_sequence)
-    plt.title("Capital Sequence over Time")
-    plt.xlabel("Steps")
-    plt.ylabel("Cash")
+    cash_sequence = run_dice_simulation()
+    plt.plot(cash_sequence)
     plt.show()
